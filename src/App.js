09f8bd45c230baebb38a9558ba36de1a42e698f5
Mansir <mansiraziz93@gmail.com>
2023-07-17 20:32:04 +0000

    update...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import "./App.css";
-import Body from "./components/Body";
-import Header from "./components/Header";
-import Slider from "./components/Slider";
 import MoviePage from "./components/MoviesPage";
 import Movies from "./components/Movies";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -9,15 +6,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={Movies} />
-          <Route path="/movies/:id" element={MoviePage} />
-        </Routes>
-      </BrowserRouter>
-      <Header />
-      <Slider />
-      <Body />
+      <Routes>
+        <Route exact path="/" element={<Movies />} />
+        <Route path="/movies/:id" element={<MoviePage />} />
+      </Routes>
     </div>
   );
 }
